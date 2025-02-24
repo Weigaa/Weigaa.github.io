11ffc652818a34f8814a4ed87cc6fcdf7fbbd699
Weigaa <38322329+Weigaa@users.noreply.github.com>
2025-02-24 12:07:06 +0000

    Deploying to gh-pages from @ Weigaa/Weigaa.github.io@9db0c2b0646eeba77b32abb93a0267ca1ec52d80 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -32,7 +32,7 @@ ninja.data = [{
           },
         },{id: "nav-cv",
           title: "cv",
-          description: "This is a description of the page. You can modify it in &#39;_pages/cv.md&#39;. You can also change or remove the top pdf download button.",
+          description: "Welcome to my personal CV where you can discover more about my research interests and personal experiences.",
           section: "Navigation",
           handler: () => {
             window.location.href = "/cv/";
